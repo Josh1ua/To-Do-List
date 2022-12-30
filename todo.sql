445fa112acdb63a401e8b2e13df97f9f806941db
Joshua <jswabiswakarma@outlook.com>
2022-12-30 17:13:23 +0530

    second commit

--- todo.sql
+++ todo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 29, 2022 at 06:31 PM
+-- Generation Time: Dec 30, 2022 at 12:42 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -37,7 +37,14 @@ CREATE TABLE `Tasks` (
 INSERT INTO `Tasks` (`taid`, `email`, `Task_Name`, `Date`, `Time`, `Description`) VALUES
 (1, 'jswabiswakarma@gmail.com', 'one', '2022-12-22', '06:05:00', 'JSosajcbiac'),
 (2, 'jswabiswakarma@gmail.com', 'two', '2022-12-12', '06:05:00', 'Biswaaera'),
-(3, 'jswabiswakarma@gmail.com', 'three', '2022-12-06', '09:08:00', 'Ksakxcj');
+(3, 'jswabiswakarma@gmail.com', 'three', '2022-12-06', '09:08:00', 'Ksakxcj'),
+(4, 'todo@gmail.com', 'Add`', '2022-12-30', '05:00:00', 'Add on hover zoom and shjow timer countdown'),
+(5, 'todo@gmail.com', 'design', '2022-12-30', '17:00:00', 'grid items must look beautiful '),
+(6, 'todo@gmail.com', 'countdown', '2022-12-30', '17:00:00', 'figure out the java script date function and create countdown timer out of it\r\n'),
+(7, 'todo@gmail.com', 'samde', '2022-12-30', '17:00:00', 'Every thing must be same size for grid items'),
+(8, 'todo@gmail.com', 'center ', '2022-12-30', '17:00:00', 'center grid container'),
+(9, 'todo@gmail.com', 'readme', '2022-12-30', '22:00:00', 'create readme file with screen shots and update all to github'),
+(10, 'todo@gmail.com', 'label `', '2022-12-30', '17:00:00', 'change colours of the grid according to its priority');
 
 -- --------------------------------------------------------
 
@@ -49,19 +56,18 @@ CREATE TABLE `users` (
   `uid` int(11) NOT NULL,
   `name` text NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `contact no` varchar(255) NOT NULL,
-  `Address` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`uid`, `name`, `email`, `password`, `contact no`, `Address`) VALUES
-(1, 'joasocac askcacnnk', 'jswabiswakarma@outlook.com', '456789', 'acac', ''),
-(2, 'Joshua Biswakarma', 'jswabiswakarma@gmail.com', '123456', 'lkasfnalc', ''),
-(3, 'joshsa acskack', 'biacbac@gmail.com', '456789', 'akxcbjac', '');
+INSERT INTO `users` (`uid`, `name`, `email`, `password`) VALUES
+(1, 'joasocac askcacnnk', 'jswabiswakarma@outlook.com', '456789'),
+(2, 'Joshua Biswakarma', 'jswabiswakarma@gmail.com', '123456'),
+(3, 'joshsa acskack', 'biacbac@gmail.com', '456789'),
+(4, 'Todo ok', 'todo@gmail.com', '123456');
 
 --
 -- Indexes for dumped tables
